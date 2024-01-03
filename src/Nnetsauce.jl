4f9d4e69030128f1948a8293b42ba8f9ec84d8e1
thierrymoudiki <thierry.moudiki@gmail.com>
2024-01-03 19:10:29 +0100

    use ; in function

--- src/Nnetsauce.jl
+++ src/Nnetsauce.jl
@@ -13,6 +13,19 @@ module Nnetsauce
     Conda.pip("install", "nnetsauce")  # Install nnetsauce
     ns = PyCall.pyimport("nnetsauce")
 
-    export ns.Ridge2Classifier, ns.Ridge2MultitaskClassifier
+    function Ridge2Classifier(;n_hidden_features=100, dropout=0.15, n_clusters=1, 
+        lambda1=1.0, lambda2=1.0)
+        return ns.Ridge2Classifier(n_hidden_features=n_hidden_features, dropout=dropout, 
+            n_clusters=n_clusters, lambda1=lambda1, lambda2=lambda2)
+    end
+
+    function Ridge2MultitaskClassifier(;n_hidden_features=100, dropout=0.15, n_clusters=1, 
+        lambda1=1.0, lambda2=1.0)
+        return ns.Ridge2MultitaskClassifier(n_hidden_features=n_hidden_features, dropout=dropout, 
+            n_clusters=n_clusters, lambda1=lambda1, lambda2=lambda2)        
+    end
+
+    export Ridge2Classifier
+    export Ridge2MultitaskClassifier
 
 end
