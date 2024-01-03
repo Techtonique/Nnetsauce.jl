e6fab5376891ae8e8f957776dd8f4fda4470ff24
thierrymoudiki <thierry.moudiki@gmail.com>
2024-01-03 11:09:08 +0100

    include dependency with PyCall

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,8 +14,12 @@ Conda.pip_interop(true)  # Enable pip interop
 Conda.pip("install", "scikit-learn")  # Install scikit-learn
 Conda.add("numpy")
 
-np = pyimport("numpy")
-sklearn = pyimport("sklearn")
+using PyCall
+
+np = PyCall.pyimport("numpy")
+sklearn = PyCall.pyimport("sklearn")
+
+using Nnetsauce
 
 @testset "Nnetsauce.jl" begin
     @testset "Ridge2Classifier" begin
@@ -25,7 +29,7 @@ sklearn = pyimport("sklearn")
         y = dataset["target"]
         X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, 
         test_size=0.2, random_state=123)
-        clf = Ridge2Classifier(n_hidden_features=9, dropout=0.43, n_clusters=1, 
+        clf = Nnetsauce.Ridge2Classifier(n_hidden_features=9, dropout=0.43, n_clusters=1, 
         lambda1=1.24023438e+01, lambda2=7.30263672e+03)
         clf.fit(X=X_train, y=y_train)        
         @test clf.score(X_test, y_test) >= 0.98
