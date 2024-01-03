96ceab1dbb6ba70996f6f5207af45a42e67b415f
thierrymoudiki <thierry.moudiki@gmail.com>
2024-01-03 18:54:03 +0100

    pip install nnetsauce

--- src/Nnetsauce.jl
+++ src/Nnetsauce.jl
@@ -3,9 +3,14 @@ module Nnetsauce
     import Pkg
 
     Pkg.add("PyCall")
-
+    Pkg.add("Conda")
+    
     import PyCall
+    import Conda
 
+    Conda.add("pip")  # Ensure pip is installed
+    Conda.pip_interop(true)  # Enable pip interop
+    Conda.pip("install", "nnetsauce")  # Install nnetsauce
     ns = PyCall.pyimport("nnetsauce")
 
     export ns.Ridge2Classifier, ns.Ridge2MultitaskClassifier
